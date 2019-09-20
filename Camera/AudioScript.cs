466e4d4f34d26e9a67822ad189b24e81926e97cb
Josh <joshuacho@utexas.edu>
2019-09-20 14:44:23 -0500

    Add files via upload

---
+++ Camera/AudioScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+
+
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+  
+
+
+    void Start()
+    {
+       MusicSource.clip = MusicClip;
+    }
+
+    
+
+
+
+    public void MainSongOn ()   
+    {
+        MusicSource.Play();
+        DontDestroyOnLoad(transform.gameObject);       //In order to not destroy audio, audio file MUST be in the root of hierarchy
+        
+    }
+
+
+
+    public void MainSongOff()
+    {
+        MusicSource.Stop();
+
+    }
+
+
+}
