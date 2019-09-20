687fa03ea2a3ad8223bf1f4d51323d591d7faa12
Josh <joshuacho@utexas.edu>
2019-09-20 14:43:46 -0500

    Add files via upload

---
+++ Effects/DontDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+
+
+
+{
+
+    void awake()
+    {
+
+        DontDestroyOnLoad(transform.gameObject);
+
+    }
+
+
+   
+}
